35875af550fee5ff023b6036c6d6ba3fbc3d3541
yongqi <yongqi_lee@yahoo.com>
2018-03-09 21:08:44 +0800

    fix a bug

--- labelme/app.py
+++ labelme/app.py
@@ -38,7 +38,7 @@ except ImportError:
     from PyQt4.QtCore import *
     PYQT5 = False
 
-from labelme import resources
+#from labelme import resources
 from labelme.lib import struct, newAction, newIcon, addActions, fmtShortcut
 from labelme.shape import Shape, DEFAULT_LINE_COLOR, DEFAULT_FILL_COLOR
 from labelme.canvas import Canvas
